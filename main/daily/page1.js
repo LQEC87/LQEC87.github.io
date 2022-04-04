622ecb90cb254d17daa170af94bcf2216b24441f
LQEC87 <sa10shi.seki@gmail.com>
2022-04-04 12:08:49 +0900

    Update page1.js

--- main/daily/page1.js
+++ main/daily/page1.js
@@ -1,15 +1,5 @@
 const myHeading = document.querySelector('.text');
 const DefaultText = myHeading.textContent;
-document.querySelector('#tested').onclick = function(){
-    myHeading.textContent = 'Hello, world!';
-};
-document.querySelector('#no').onclick = function(){
-    myHeading.textContent = 'You have to test, so please check javascript.com';
-};
-document.querySelector('#reset').onclick = function(){
-    myHeading.textContent = DefaultText;
-};
-
 const countdisplay = document.querySelector('.count');
 var count = 0;
 function data(updown) {
@@ -17,13 +7,27 @@ function data(updown) {
         count--;
     } else {
         count++;
-    }
+    };
     if(((count % 100) === 0) && (count !== 0)){
         return 'good';
     } else {
         return count;
     };
-}
+};
+
+document.querySelector('#tested').onclick = function(){
+    myHeading.textContent = 'Hello, world!';
+};
+document.querySelector('#no').onclick = function(){
+    myHeading.textContent = 'You have to test, so please check javascript.com';
+};
+
+document.querySelector('#reset').onclick = function(){
+    myHeading.textContent = DefaultText;
+    count = 0;
+    countdisplay.textContent = 'count:0';
+};
+
 document.getElementById('dataINC').onclick = function(){
     countdisplay.textContent = 'count:' + data(true);
 };
