09c827a42e3ad9f26291c5c6751be64867e25324
LQEC87 <73701602+LQEC87@users.noreply.github.com>
2022-09-15 16:12:52 +0900

    Create index.js

---
+++ daily/prougramming/index.js
@@ -0,0 +1,22 @@
+var Fapi = Boolern(window.File)
+if (Fapi) {
+  var fileInfomationFunc = function(){
+    var files = import("day1.html");
+    const info = document.querySelector('#day1 #info');
+    info.textContent = "FileName:" + files.name + "<br />FileType:" + files.type + "FileSize:" + files.size + " Byte<br />";
+  }
+  window.addEventListener('load', fileInfomationFunc);
+}
+
+
+
+var scrollAnimationElm = document.querySelectorAll('.scani');
+var scrollAnimationFunc = function() {
+  for(var i = 0; i < scrollAnimationElm.length; i++) {
+    if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + 300) {
+      scrollAnimationElm[i].classList.add('show');
+    }
+  }
+}
+window.addEventListener('load', scrollAnimationFunc);
+window.addEventListener('scroll', scrollAnimationFunc);
